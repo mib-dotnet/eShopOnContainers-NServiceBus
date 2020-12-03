506f081200b265832154fcd178bca030e832b73e
muralib91dotnet <muralib91dev@hotmail.com>
2020-12-03 12:33:02 -0800

    Identity.API - Appsettings file explicit loading disabled to support load settings from docker-compose as well & ConnectionString change in docker-compose

--- src/Services/Identity/Identity.API/Program.cs
+++ src/Services/Identity/Identity.API/Program.cs
@@ -1,21 +1,44 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using System.IO;
 
 namespace eShopOnContainers.Identity
 {
+ 
     public class Program
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseHealthChecks("/hc")
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+            BuildWebHost(args).Run();
+        }
 
-            host.Run();
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var builder = WebHost.CreateDefaultBuilder(args)
+                                .ConfigureAppConfiguration(ConfigureAppDefaultConfiguration)
+                                .UseStartup<Startup>()
+                                .UseHealthChecks("/hc")
+                                .UseContentRoot(Directory.GetCurrentDirectory())
+                                .Build();
+
+            return builder;
+        }
+
+        private static void ConfigureAppDefaultConfiguration(WebHostBuilderContext hostingContext, IConfigurationBuilder builder)
+        {
+            var env = hostingContext.HostingEnvironment;
+
+            if (env.IsDevelopment())
+            {
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets<Startup>();
+            }
+
+            //builder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            //    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+            //builder.AddEnvironmentVariables();
         }
     }
 }
