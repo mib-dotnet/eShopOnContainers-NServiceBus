f26ef5018d22937a920d2a7ce535ea5a307d55ad
muralib91dotnet <muralib91dev@hotmail.com>
2020-12-05 12:39:30 -0800

    WebMVC app - Docker startup delayed

--- src/Web/WebMVC/exec_webmvc.sh
+++ src/Web/WebMVC/exec_webmvc.sh
@@ -1,6 +1,9 @@
 ﻿echo --------------------------
-echo 💥Hello World💥
+echo 💥WebMVC💥
+echo --------------------------
 ls --all
 echo --------------------------
-dockerize -wait tcp://sql.data -timeout 300s -wait-retry-interval 30s && dotnet WebMVC.dll
+sleep 100
+dockerize -wait tcp://sql.data:1433 -wait http://rabbitmq -timeout 60s -wait-retry-interval 90s tail -f /dev/null
+# dockerize -wait tcp://sql.data:1433 -wait http://sql.data -timeout 50s -wait-retry-interval 20s dotnet Catalog.API.dll
 echo --------------------------
