042b10768e1c75a7bf2346b4107e261f4bf6051e
muralib91dotnet <muralib91dev@hotmail.com>
2020-12-09 12:16:12 -0800

    Dockerize wait tool Added - for remaining APIs: To delay Docker containers Startup after there dependencies are started

---
+++ src/Services/Location/Locations.API/exec_locationsapi.sh
@@ -0,0 +1,10 @@
+echo --------------------------
+echo 💥 Locations.API 💥
+echo --------------------------
+ls --all
+echo --------------------------
+# dockerize -wait tcp://sql.data:1433 -timeout 300s -wait-retry-interval 30s && dotnet Identity.API.dll
+# dockerize -wait tcp://sql.data:1433 -wait http://sql.data -timeout 50s -wait-retry-interval 20s tail -f /dev/null
+sleep 100
+dockerize -wait tcp://sql.data:1433 -wait tcp://nosql.data:27017 -wait http://rabbitmq:15672 -timeout 60s -wait-retry-interval 90s tail -f /dev/null
+echo --------------------------
