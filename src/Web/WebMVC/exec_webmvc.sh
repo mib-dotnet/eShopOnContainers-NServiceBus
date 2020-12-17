f17b4f45cd2ebb962c9814bd8585bc4f20ecf8af
muralib91dotnet <muralib91dev@hotmail.com>
2020-12-17 12:54:45 -0800

    WebMVC - Docker Startup delay scripts - refactored & Function imports implemented

--- src/Web/WebMVC/exec_webmvc.sh
+++ src/Web/WebMVC/exec_webmvc.sh
@@ -1,8 +1,34 @@
-﻿echo --------------------------
+﻿#!/bin/bash
+
+#file imports
+source ./await-container-dependencies.sh
+
+echo --------------------------
 echo 💥WebMVC💥
 echo --------------------------
 ls --all
 echo --------------------------
-sleep 100
-dockerize -wait tcp://sql.data:1433 -wait http://catalog.api -wait http://ordering.api -wait http://identity.api -wait http://basket.api -wait http://marketing.api -timeout 60s -wait-retry-interval 90s tail -f /dev/null
+sleep 40
+# dockerize -wait tcp://sql.data:1433 -wait http://catalog.api -wait http://ordering.api -wait http://identity.api -wait http://basket.api -wait http://marketing.api -timeout 60s -wait-retry-interval 90s tail -f /dev/null
+checkDependencyOthers sql.data:1433 15
+
+checkDependencyHTTP catalog.api 15
+
+checkDependencyHTTP identity.api 15
+
+checkDependencyHTTP ordering.api 15
+
+checkDependencyHTTP basket.api 15
+
+checkDependencyHTTP marketing.api 15
+
+checkDependencyHTTP rabbitmq:15672 15
+
+
+
+# Release Mode
+# dotnet WebMVC.dll
+
+# Debug Mode
+tail -f /dev/null
 echo --------------------------
